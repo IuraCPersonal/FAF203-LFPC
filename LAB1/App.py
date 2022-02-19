0c62e2fcaa5bd215c40b6b628bf74126a68e9d29
Iura <47008227+IuraCPersonal@users.noreply.github.com>
2022-02-19 15:31:14 +0200

    fixed the states

--- LAB1/App.py
+++ LAB1/App.py
@@ -1,14 +1,13 @@
 # Importing all needed libraries.
 import os
-import random
 import graphviz as gz
 
 # Add Graphviz to the PATH.
 os.environ["PATH"] += os.pathsep + 'C:\\Program Files\\Graphviz\\bin'
 
 # Setup the Grammar.
-terminal_symbols = set(['a', 'b', 'c', 'd', 'e', 'f', 'j'])
-non_terminal_symbols = set(['S', 'L', 'D'])
+terminal_symbols = ['a', 'b', 'c', 'd', 'e', 'f', 'j']
+non_terminal_symbols = ['S', 'L', 'D']
 
 # Setup the Set of Productions of Rules.
 set_of_productions = tuple([
@@ -25,6 +24,16 @@ set_of_productions = tuple([
     ('D', 'd')
 ])
 
+# Make the link between the states
+g_dict = {}
+count = 0
+
+for element in non_terminal_symbols:
+    g_dict[element] = "q{}".format(count)
+    count += 1
+
+g_dict['Q'] = "q{}".format(count + 1)
+
 # Empty Dictionary.
 __set_of_productions = {}
 
@@ -38,25 +47,23 @@ for nonterminal in set_of_productions:
 
 # Computing the Finite Automaton (FA).
 finite_automaton = dict()
-for production in __set_of_productions:
-    finite_automaton[production] = []
-    for result in __set_of_productions[production]:
-        if len(result) == 2:
-            # random.randint(0, 100000) is just a dummy way to work around dublicates.
-            # finite_automaton.update(
-            #     {(production, result[0], random.randint(0, 100000)): result[1]})
-            finite_automaton[production].append((result[0], result[1]))
+for non_terminal in __set_of_productions:
+    finite_automaton[g_dict[non_terminal]] = []
+    for transition in __set_of_productions[non_terminal]:
+        if len(transition) == 2:
+            finite_automaton[g_dict[non_terminal]].append(
+                (transition[0], g_dict[transition[1]]))
         else:
-            # finite_automaton.update(
-            #     {(production, result, random.randint(0, 100000)): 'Q'})
-            finite_automaton[production].append((result, 'Q'))
+            finite_automaton[g_dict[non_terminal]].append((transition, 'Q'))
 
 
 # DEBUG:
+print('Finite Automaton:')
 print(finite_automaton)
 
+
 # Function to check if the given string belongs to the language L(G).
-def is_accepted(string, adjacency_matrix, start_node='S'):
+def is_accepted(string, adjacency_matrix, start_node='q0'):
     # Set the current node to the start one.
     current_node = start_node
     for c in string:
@@ -69,18 +76,22 @@ def is_accepted(string, adjacency_matrix, start_node='S'):
                 break
         else:
             return False
-    
+
+    # why NFA
     # Check if the last node is other then Empty.
     if current_node != 'Q':
         for prod in adjacency_matrix[current_node]:
-            # If there exists a prod with the exact weight like the last character and the adjacency node empty.
+            # If there exists a prod with the exact weight like the last character
+            # and the adjacency node empty.
             if prod[0] == string[-1] and prod[1] == 'Q':
                 return True
 
     # Return True or False if the current (last) node is the empty one.
     return current_node == 'Q'
 
-print(is_accepted('aade', finite_automaton))
+
+# DEBUG:
+print('Is accepted:', is_accepted('aade', finite_automaton))
 
 # BONUS POINT:
 # Ploting the FA Graph.
@@ -89,32 +100,27 @@ print(is_accepted('aade', finite_automaton))
 G = gz.Digraph()
 G.attr(rankdir='LR', size='8,5')
 
-g_dict = {}
-count = -1
-
-for element in non_terminal_symbols:
-    count += 1
-    g_dict[element] = "q{}".format(count)
-
-g_dict['Q'] = "q{}".format(count + 1)
-# print(g_dict)
-
 # Compute all nodes.
 for element in finite_automaton:
     for weight, adj_node in finite_automaton[element]:
         G.attr('node', shape='circle')
-        G.node(g_dict[element])
+        G.node(element)
 
         # Check for the empty node.
         if adj_node == 'Q':
             G.attr('node', shape='doublecircle')
-            G.node(g_dict[adj_node])
+            G.node(adj_node)
         else:
             G.attr('node', shape='circle')
-            G.node(g_dict[adj_node])
+            G.node(adj_node)
 
         # Add the labels.
-        G.edge(g_dict[element], g_dict[adj_node], label=weight)
+        G.edge(element, adj_node, label=weight)
+
+# Add the start arrow.
+G.attr('node', shape='none')
+G.node('')
+G.edge('', 'q0')
 
 # Show/Export the Graph.
 # G.view()
